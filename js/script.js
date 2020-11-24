f25ca6de30e8c54bcec5ffd7ebfe1ec0c2ce57a2
ludmila2107 <74045520+ludmila2107@users.noreply.github.com>
2020-11-24 09:47:30 +0300

    новый код

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
+//let btnCreate = document.querySelector('.btn-create');
+//let newElements = document.querySelector('.new-elements');
+//let elem;
+//let numberElements = document.querySelector('.number-elements');
+
+
+//btnCreate.onclick = function() {
+  //  for(let i = 0; i < +numberElements.value; i++) {
+
+
+
+    //    elem = document.createElement('p');
+      //  elem.textContent = "Новый абзац...";
+    //    elem.classList.add('elem');
+
+     //   newElements.append(elem);
+ //   }
+//}
+
+
+
 let btnCreate = document.querySelector('.btn-create');
+let btnRemove = document.querySelector('.btn-remove');
 let newElements = document.querySelector('.new-elements');
 let elem;
 let numberElements = document.querySelector('.number-elements');
@@ -6,10 +28,24 @@ let numberElements = document.querySelector('.number-elements');
 
 btnCreate.onclick = function() {
     for(let i = 0; i < +numberElements.value; i++) {
+
         elem = document.createElement('p');
         elem.textContent = "Новый абзац...";
         elem.classList.add('elem');
 
+        if(i == 0) {
+            elem.style.backgroundColor = "darkred";
+            elem.style.color = "white";
+        }
+
         newElements.append(elem);
+    }
+}
+
+let children = newElements.childNodes;
+
+btnRemove.onclick = function() {
+    for(let i = children.length - 1; i > -1; i--) {
+        newElements.removeChild(children[i]);
     }
 }
