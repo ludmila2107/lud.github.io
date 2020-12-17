170d2a5b787f6fddc7142cec43d834871f135d06
Людмила <lm8023740@gmail.com>
2020-12-17 09:30:54 +0300

    Добавлено

--- card/js/script.js
+++ card/js/script.js
@@ -4,14 +4,16 @@ class Card {
     #_titleProduct;
     #_descr;
     #_price;
+    #_category = {};
     #_parent;
 
-    constructor(src, alt, titleProduct, descr, price, parent) {
+    constructor(src, alt, titleProduct, descr, price, category, parent) {
         this.src = src;
         this.alt = alt;
         this.titleProduct = titleProduct;
         this.descr = descr;
         this.price = price;
+        this.category = category;
         this.parent = document.querySelector(parent);
     }
 
@@ -55,6 +57,14 @@ class Card {
         this.#_price = priceVal;
     }
 
+    get category() {
+        return this.#_category;
+    }
+
+    set category(categoryVal) {
+        this.#_category = categoryVal;
+    }
+
     get parent() {
         return this.#_parent;
     }
@@ -66,8 +76,9 @@ class Card {
     createCard() {
         let elem = document.createElement('div');
         elem.classList.add('card__item');
+        elem.setAttribute('data-category', `${this.category['name']}`);
+        elem.setAttribute('data-price', `${this.price}`);
         elem.innerHTML = `
-        
         <img class='card__item-image' src='${this.src}' alt='${this.alt}'>
         <h2 class='card__item-title'>${this.titleProduct}</h2>
         <p class='card__item-descr'>${this.descr}</p>
@@ -84,54 +95,140 @@ class Card {
 
 let cards = [
     {
-        src: 'img/1.svg',
-        alt: 'Наушники 1',
-        title: 'Наушники 1',
+        src: 'img/headphones-1.jpg',
+        alt: 'Наушники',
+        title: 'Наушники',
         descr: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam, voluptatem?',
-        price: 1000
+        price: 1300,
+        category: {
+            id: 'headphones',
+            name: 'Наушники'
+        }
     },
     {
-        src: 'img/2.svg',
-        alt: 'Наушники 2',
-        title: 'Наушники 2',
+        src: 'img/headphones-2.jpg',
+        alt: 'Наушники',
+        title: 'Наушники',
         descr: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam, voluptatem?',
-        price: 1300
+        price: 2000,
+        category: {
+            id: 'headphones',
+            name: 'Наушники'
+        }
     },
     {
-        src: 'img/3.svg',
-        alt: 'Наушники 3',
-        title: 'Наушники 3',
+        src: 'img/soundspeaker-1.jpg',
+        alt: 'Колонки',
+        title: 'Колонки',
         descr: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam, voluptatem?',
-        price: 900
+        price: 3000,
+        category: {
+            id: 'soundspeakers',
+            name: 'Колонки'
+        }
+    },
+    {
+        src: 'img/telephone-1.jpg',
+        alt: 'Смартфон',
+        title: 'Смартфон',
+        descr: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam, voluptatem?',
+        price: 20000,
+        category: {
+            id: 'telephone',
+            name: 'Телефоны'
+        }
+    },
+    {
+        src: 'img/soundspeaker-2.jpg',
+        alt: 'Колонки',
+        title: 'Колонки',
+        descr: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam, voluptatem?',
+        price: 5000,
+        category: {
+            id: 'soundspeakers',
+            name: 'Колонки'
+        }
+    },
+    {
+        src: 'img/headphones-3.jpg',
+        alt: 'Наушники',
+        title: 'Наушники',
+        descr: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam, voluptatem?',
+        price: 900,
+        category: {
+            id: 'headphones',
+            name: 'Наушники'
+        }
     }
 ]
 
-let card1 = new Card(cards[0]['src'],
-    cards[0]['alt'],
-    cards[0]['title'],
-    cards[0]['descr'],
-    cards[0]['price'],
-    '.cards .container').createCard();
+let card;
 
-let card2 = new Card(cards[1]['src'],
-    cards[1]['alt'],
-    cards[1]['title'],
-    cards[1]['descr'],
-    cards[1]['price'],
-    '.cards .container').createCard();
+for (let i = 0; i < cards.length; i++) {
+    card = new Card(cards[i]['src'],
+        cards[i]['alt'],
+        cards[i]['title'],
+        cards[i]['descr'],
+        cards[i]['price'],
+        cards[i]['category'],
+        '.cards .container').createCard();
+}
 
-let card3 = new Card(cards[2]['src'],
-    cards[2]['alt'],
-    cards[2]['title'],
-    cards[2]['descr'],
-    cards[2]['price'],
-    '.cards .container').createCard();
+let listCategories = document.querySelector('.filters__category-list');
+let listCards = document.querySelectorAll('.card__item');
+let minPrice = document.querySelector('#minPrice');
+let maxPrice = document.querySelector('#maxPrice');
+let buttonShow = document.querySelector('.filters-button');
 
-    for(let i=0; i<cards.length;i++){
-        card = new Card(cards[i]['src'],
-            cards[i]['alt'],
-            cards[i]['title'],
-            cards[i]['descr'],
-            cards[i]['price'],
-            '.cards .container').createCard();
+minPrice.addEventListener('change', function () {
+    if (+minPrice.value <= +minPrice.dataset.min) {
+        minPrice.classList.add('filters-error');
     }
+    else {
+        minPrice.classList.remove('filters-error');
+    }
+});
+
+maxPrice.addEventListener('change', function () {
+    if (+maxPrice.value >= +maxPrice.dataset.max) {
+        maxPrice.classList.add('filters-error');
+    }
+    else {
+        maxPrice.classList.remove('filters-error');
+    }
+});
+
+buttonShow.addEventListener('click', () => { filterCards(); });
+
+function showAllCards() {
+    for (let listcard of listCards) {
+        listcard.classList.remove('card__item_hidden');
+    }
+}
+
+function filterCards() {
+
+    showAllCards();
+
+    for (let listcard of listCards) {
+        if (listcard.dataset.category != listCategories.value && listCategories.value !== 'Все' && minPrice.value == "" && maxPrice.value == "") {
+            listcard.classList.add('card__item_hidden');
+        }
+        else if (listcard.dataset.category != listCategories.value && listCategories.value !== 'Все' && minPrice.value != "" && maxPrice.value != "" && !minPrice.classList.contains('filters-error') && !maxPrice.classList.contains('filters-error')) {
+            if (+listcard.dataset.price >= +minPrice.value && +listcard.dataset.price <= +maxPrice.value) {
+                listcard.classList.add('card__item_hidden');
+            }
+        }
+    }
+}
+
+// listCategories.addEventListener('input', function () {
+//     for (let listcard of listCards) {
+//         if (listcard.dataset.category !== listCategories.value && listCategories.value !== 'Все') {
+//             listcard.classList.add('card__item_hidden');
+//         }
+//         else {
+//             listcard.classList.remove('card__item_hidden');
+//         }
+//     }
+// });
---
+++ tur/tur.js
@@ -0,0 +1,256 @@
+class Card {
+    #_src;
+    #_alt;
+    #_titleProduct;
+    #_descr;
+    #_price;
+    #_category = {};
+    #_parent;
+
+    constructor(src, alt, titleProduct, descr, price, category, parent) {
+        this.src = src;
+        this.alt = alt;
+        this.titleProduct = titleProduct;
+        this.descr = descr;
+        this.price = price;
+        this.category = category;
+        this.parent = document.querySelector(parent);
+    }
+
+    get src() {
+        return this.#_src;
+    }
+
+    set src(srcVal) {
+        this.#_src = srcVal;
+    }
+
+    get alt() {
+        return this.#_alt;
+    }
+
+    set alt(altVal) {
+        this.#_alt = altVal;
+    }
+
+    get titleProduct() {
+        return this.#_titleProduct;
+    }
+
+    set titleProduct(titleVal) {
+        this.#_titleProduct = titleVal;
+    }
+
+    get descr() {
+        return this.#_descr;
+    }
+
+    set descr(descrVal) {
+        this.#_descr = descrVal;
+    }
+
+    get price() {
+        return this.#_price;
+    }
+
+    set price(priceVal) {
+        this.#_price = priceVal;
+    }
+
+    get category() {
+        return this.#_category;
+    }
+
+    set category(categoryVal) {
+        this.#_category = categoryVal;
+    }
+
+    get parent() {
+        return this.#_parent;
+    }
+
+    set parent(parentVal) {
+        this.#_parent = parentVal;
+    }
+
+    createCard() {
+        let elem = document.createElement('div');
+        elem.classList.add('card__item');
+        elem.setAttribute('data-category', `${this.category['name']}`);
+        elem.setAttribute('data-price', `${this.price}`);
+        elem.innerHTML = `
+        <img class='card__item-image' src='${this.src}' alt='${this.alt}'>
+        <h2 class='card__item-title'>${this.titleProduct}</h2>
+        <p class='card__item-descr'>${this.descr}</p>
+        <div class='card__item__price'>
+            <span class='card__item__price-title'>Цена:</span>
+            <b class='card__item__price-value'>${this.price} €.</b> 
+        </div>
+        `;
+
+        this.parent.append(elem);
+    }
+}
+
+
+let cards = [
+    {
+        src: 'img/krit1.jpg',
+        alt: 'Крит',
+        title: 'Крит',
+        descr: 'Таинственная родина минотавра, самый большой греческий остров',
+        price: 1300,
+        category: {
+            id: 'excursion',
+            name: 'Экскурсионные'
+        }
+    },
+    {
+        src: 'img/rodos1.jpg',
+        alt: 'Родос',
+        title: 'Родос',
+        descr: 'Остров рыцарей. Средневековые стены, построенные рыцарями Святого Иоанна, окружающие Старый город и прекрасные Византийские  церквm.',
+        price: 1000,
+        category: {
+            id: 'weekend',
+            name: 'Туры выходного дня'
+        }
+    },
+    {
+        src: 'img/korfy1.jpg',
+        alt: 'Корфу',
+        title: 'Корфу',
+        descr: 'Один из самых дорогих греческих курортов. Здесь множество маленьких бухт и заливов.',
+        price: 3000,
+        category: {
+            id: 'beach',
+            name: 'Пляжные'
+        }
+    },
+    {
+        src: 'img/halkidiki1.jpg',
+        alt: 'Халкидики',
+        title: 'Халкидики',
+        descr: 'Чрезвычайно популярный курорт в летние месяцы. ',
+        price: 20000,
+        category: {
+            id: 'children',
+            name: 'Отдых с детьми'
+        }
+    },
+    {
+        src: 'img/santorini1.jpg',
+        alt: 'Санторини',
+        title: 'Санторини',
+        descr: 'Черный песок на вулканических пляжах, голубые и синие купола церквей, белоснежные стены домов. Не отпуск, а сказка.',
+        price: 10000,
+        category: {
+            id: 'beach',
+            name: 'Пляжные'
+        }
+    },
+    {
+        src: 'img/zakinf1.jpg',
+        alt: 'Закинф',
+        title: 'Закинф',
+        descr: 'Этот ионический остров славится своей пышной растительностью, которая дала ему название «цветок Востока» (Фиоре ди Леванте).',
+        price: 60000,
+        category: {
+            id: 'wedding',
+            name: 'Свадебные'
+        }
+    },
+    {
+        src: 'img/kos1.jpg',
+        alt: 'Кос',
+        title: 'Кос',
+        descr: 'Славится великолепной природой, уютными пляжами, чистым морем и условиями для виндсерфинга.',
+        price: 10000,
+        category: {
+            id: 'weekend',
+            name: 'Туры выходного дня'
+        }
+    },
+    {
+        src: 'img/miconos1.jpg',
+        alt: 'Миконос',
+        title: 'Миконос',
+        descr: 'Самый космополитичный курорт Греции, Миконос, часто посещают известные голливудские звезды.',
+        price: 90000,
+        category: {
+            id: 'beach',
+            name: 'Пляжные'
+        }
+    },
+    {
+        src: 'img/paros1.jpg',
+        alt: 'Парос',
+        title: 'Парос',
+        descr: 'Это прекрасный курорт Киклад, славится традиционными часовнями, чистыми пляжами и захватывающей ночной жизнью.',
+        price: 90000,
+        category: {
+            id: 'beach',
+            name: 'Пляжные'
+        }
+    },
+]
+
+let card;
+
+for (let i = 0; i < cards.length; i++) {
+    card = new Card(cards[i]['src'],
+        cards[i]['alt'],
+        cards[i]['title'],
+        cards[i]['descr'],
+        cards[i]['price'],
+        cards[i]['category'],
+        '.cards .container').createCard();
+}
+
+let listCategories = document.querySelector('.filters__category-list');
+let listCards = document.querySelectorAll('.card__item');
+let minPrice = document.querySelector('#minPrice');
+let maxPrice = document.querySelector('#maxPrice');
+let buttonShow = document.querySelector('.filters-button');
+
+minPrice.addEventListener('change', function () {
+    if (+minPrice.value <= +minPrice.dataset.min) {
+        minPrice.classList.add('filters-error');
+    }
+    else {
+        minPrice.classList.remove('filters-error');
+    }
+});
+
+maxPrice.addEventListener('change', function () {
+    if (+maxPrice.value >= +maxPrice.dataset.max) {
+        maxPrice.classList.add('filters-error');
+    }
+    else {
+        maxPrice.classList.remove('filters-error');
+    }
+});
+
+buttonShow.addEventListener('click', () => { filterCards(); });
+
+function showAllCards() {
+    for (let listcard of listCards) {
+        listcard.classList.remove('card__item_hidden');
+    }
+}
+
+function filterCards() {
+
+    showAllCards();
+
+    for (let listcard of listCards) {
+        if (listcard.dataset.category != listCategories.value && listCategories.value !== 'Все' && minPrice.value == "" && maxPrice.value == "") {
+            listcard.classList.add('card__item_hidden');
+        }
+        else if (listcard.dataset.category != listCategories.value && listCategories.value !== 'Все' && minPrice.value != "" && maxPrice.value != "" && !minPrice.classList.contains('filters-error') && !maxPrice.classList.contains('filters-error')) {
+            if (+listcard.dataset.price >= +minPrice.value && +listcard.dataset.price <= +maxPrice.value) {
+                listcard.classList.add('card__item_hidden');
+            }
+        }
+    }
+}
